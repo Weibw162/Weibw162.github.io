9db7b61c41a6a7e2637aa06e4a32d5e5a8456d72
weibw <weibw162@163.com>
2021-12-14 07:51:18 +0800

    调整footer

--- theme-vdoing/components/Footer.vue
+++ theme-vdoing/components/Footer.vue
@@ -19,7 +19,7 @@
       title="本站主题"
     >Vdoing</a>
     <template v-if="footer">
-      | Copyright © {{ footer.createYear }}-{{ new Date().getFullYear() }}s
+      | Copyright © {{ footer.createYear }}-{{ new Date().getFullYear() }}
       <span
         v-html="footer.copyrightInfo"
       ></span>
